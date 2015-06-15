07d0c83f40db7f4cfbe48f44eff0fd726f6073af
Eric Enns <eric.enns@phac-aspc.gc.ca>
2015-06-15 10:58:58 -0500

    Added in initial structure for program. Using click to emulate samtools like command structure, have included an example hello world subcommand to help with development.

---
+++ quasitools/cli.py
@@ -0,0 +1,76 @@
+"""
+Copyright Government of Canada 2015
+
+Written by: Eric Enns, National Microbiology Laboratory, Public Health Agency of Canada
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this work except in compliance with the License. You may obtain a copy of the
+License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+import os
+import sys
+import click
+
+
+CONTEXT_SETTINGS = dict(auto_envvar_prefix='QUASITOOLS')
+
+
+class Context(object):
+
+    def __init__(self):
+        self.verbose = False
+
+    def log(self, msg, *args):
+        """Logs a message to stderr."""
+        if args:
+            msg %= args
+        click.echo(msg, file=sys.stderr)
+
+    def vlog(self, msg, *args):
+        """Logs a message to stderr only if verbose is enabled."""
+        if self.verbose:
+            self.log(msg, *args)
+
+
+pass_context = click.make_pass_decorator(Context, ensure=True)
+cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                          'commands'))
+
+
+class QuasiToolsCLI(click.MultiCommand):
+
+    def list_commands(self, ctx):
+        rv = []
+        for filename in os.listdir(cmd_folder):
+            if filename.endswith('.py') and \
+               filename.startswith('cmd_'):
+                rv.append(filename[4:-3])
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, name):
+        try:
+            if sys.version_info[0] == 2:
+                name = name.encode('ascii', 'replace')
+            mod = __import__('quasitools.commands.cmd_' + name,
+                             None, None, ['cli'])
+        except ImportError:
+            return
+        return mod.cli
+
+
+@click.command(cls=QuasiToolsCLI, context_settings=CONTEXT_SETTINGS)
+@click.option('-v', '--verbose', is_flag=True,
+              help='Enables verbose mode.')
+@pass_context
+def cli(ctx, verbose):
+    """A collection of tools for Viral Quasispecies."""
+    ctx.verbose = verbose
---
+++ setup.py
@@ -0,0 +1,47 @@
+"""
+Copyright Government of Canada 2015
+
+Written by: Eric Enns, National Microbiology Laboratory, Public Health Agency of Canada
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this work except in compliance with the License. You may obtain a copy of the
+License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='quasitools',
+    version='0.0.0',
+    url='http://gitlab.corefacility.ca/eric/quasitools.git',
+    license='Apache License, Version 2.0',
+    author='Eric Enns',
+    author_email='eric.enns@gmail.com',
+    description='Quasitools is a collection of tools for analysing Viral Quasispecies',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'quasitools = quasitools.cli:cli',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+    ]
+)
