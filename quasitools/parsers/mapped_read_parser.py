0cf37a9a2fbcfb6ab2d129fa6cbf2b037fdc2c2e
Eric Enns <eric.enns@phac-aspc.gc.ca>
2017-01-27 14:56:45 -0600

    Added in constants to mapped_read parser

--- quasitools/parsers/mapped_read_parser.py
+++ quasitools/parsers/mapped_read_parser.py
@@ -20,6 +20,9 @@ from quasitools.utilities import sam_alignment_to_padded_alignment, \
     pairwise_alignment_to_differences
 from quasitools.mapped_read import MappedRead, MappedReadCollection
 
+REVERSE_COMPLEMENTED = 16
+FORWARD = '+'
+REVERSE = '-'
 
 def parse_mapped_reads_from_bam(reference, bam):
     """Parse MappedRead mrcects from a bam file and produce a MappedReadCollection.
@@ -31,9 +34,9 @@ def parse_mapped_reads_from_bam(reference, bam):
         padded_alignment = sam_alignment_to_padded_alignment(alignment,
                                                              reference)
 
-        direct = '+'
-        if alignment.flag & 16:
-            direct = '-'
+        direct = FORWARD
+        if alignment.flag & REVERSE_COMPLEMENTED:
+            direct = REVERSE
         # TODO only calculate differences when identity < 100
         differences = pairwise_alignment_to_differences(
             padded_alignment[0], padded_alignment[2],
@@ -47,7 +50,7 @@ def parse_mapped_reads_from_bam(reference, bam):
                                  alignment.reference_end-1,
                                  direct)
         read_id = "{0}_{1}".format(alignment.query_name,
-                                   '1' if direct == '+' else '2')
+                                   '1' if direct == FORWARD else '2')
         mrc.mapped_reads[read_id] = mapped_read
 
     return mrc
