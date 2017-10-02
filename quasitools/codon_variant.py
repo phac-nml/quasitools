4104a808d55913e001e6136b79a1e5178e638604
camytran <camy.t.tran@gmail.com>
2017-10-02 14:24:33 -0500

    Add CodonVariant class and refactor cmd_mutanttypes to use new class

--- quasitools/codon_variant.py
+++ quasitools/codon_variant.py
@@ -15,26 +15,29 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
+import pdb
 from collections import defaultdict
 from quasitools.aa_census import CONFIDENT, UNCONFIDENT
 from quasitools.variant import Variant, VariantCollection
 from numpy import array as np_array
+from Bio.Seq import Seq
+
 
 class CodonVariant(Variant):
 
-    def __init__(self, gene=".", gene_start=0, gene_end=0,,
-                 codon_start=0, codon_end=0,
+    def __init__(self, gene=".", nt_start_gene=0, nt_end_gene=0,
+                 nt_start=0, nt_end=0,
                  ref_codon=".", mutant_codon=".",
                  ref_aa=".", mutant_aa=".", coverage=0,
                  mutant_freq="0", mutant_type=".",
-                 ns_count=0, s_count=0)
-        super(CodonVariant, self).__init__(*kwargs)
+                 ns_count=0, s_count=0, **kwargs):
+        super(CodonVariant, self).__init__(**kwargs)
 
         self.gene = gene
-        self.gene_start = gene_start
-        self.gene_end = gene_end
-        self.codon_start = codon_start
-        self.codon_end = codon_end
+        self.nt_start_gene = nt_start_gene
+        self.nt_end_gene = nt_end_gene
+        self.nt_start = nt_start
+        self.nt_end = nt_end
         self.ref_codon = ref_codon
         self.mutant_codon = mutant_codon
         self.ref_aa = ref_aa
@@ -45,14 +48,15 @@ class CodonVariant(Variant):
         self.ns_count = ns_count
         self.s_count = s_count
 
-    def to_csv_entry(self):
+    def to_csv_entry(self, offset):
         return "%s,%i-%i,%i,%i,%s,%s,%s,%s,%i,%.2f,%s,%0.4f,%0.4f\n" % (
-            self.gene, self.gene_start, self.gene_end,
-            self.codon_start, self.codon_end, self.ref_codon,
+            self.gene, self.nt_start_gene+offset, self.nt_end_gene+offset,
+            self.nt_start+offset, self.nt_end+offset, self.ref_codon,
             self.mutant_codon, self.ref_aa, self.mutant_aa,
             self.coverage, self.mutant_freq, self.mutant_type,
             self.ns_count, self.s_count
         )
+
 
 class CodonVariantCollection(VariantCollection):
 
@@ -62,7 +66,7 @@ class CodonVariantCollection(VariantCollection):
             lambda: defaultdict(lambda: defaultdict(dict)))
 
     @classmethod
-    def from_aa_census(cls, aa_census, *args):
+    def from_aacensus(cls, aa_census, *args):
         """Build the CodonVariantCollection from any number of
         AACensus objects"""
         
@@ -71,19 +75,106 @@ class CodonVariantCollection(VariantCollection):
 
         var_collect = cls(aa_census)
 
+        codon_permutations = [[[0]], [[0,1],[1,0]], [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]]]
+
+
         # Build up the collection of CodonVariants from many census
         for census_ind, census in enumerate(aa_census):
 
             # For each gene in this census
-            for gene key in census.genes:
+            for gene_key in census.genes:
                 gene = census.genes[gene_key]
                 frame = gene['frame']
+                codon_start = int((gene['start'] - frame) / 3) # don't understand the diff starts
+                codon_end = int((gene['end'] - frame - 2) / 3)
+                chrom = gene['chrom']
 
+                for ref_codon_pos in range(codon_start, codon_end):
+                    coverage = census.coverage_at(frame, ref_codon_pos)
+                    ref_seq = census.mapped_read_collections[0].reference.seq
+                    ref_codon = ref_seq[(ref_codon_pos*3+frame): (ref_codon_pos*3+frame) + 3].lower()
+                    ref_aa = Seq(ref_codon).translate()[0]
 
+                    for aa in census.aminos_at(frame, ref_codon_pos, CONFIDENT):
+                        frequency = census.amino_frequency_at(frame, ref_codon_pos, aa, CONFIDENT)
+                        if frequency >= 0.01:
+                            # if (ref_codon_pos*3 + frame) == 7145:
+                            #     pdb.set_trace()
+                            for codon in census.amino_to_codons_at(frame, ref_codon_pos, aa, CONFIDENT):
+                                # pdb.set_trace()
+                                if codon != ref_codon:
+                                    if aa == ref_aa:
+                                        mutation_type = "S"
+                                    else:
+                                        mutation_type = "NS"
 
+                                    base_change_count = sum(1 for c in codon if not c.islower())
+                                    base_change_pos = []
 
+                                    for codon_pos in range(0, 3):
+                                        nucleotide = codon[codon_pos:codon_pos+1]
+                                        if nucleotide.upper() == nucleotide:
+                                            base_change_pos.append(codon_pos)
 
+                                    ns_count = 0
+                                    s_count = 0
 
+                                    for codon_permutation in codon_permutations[base_change_count-1]:
+                                        codon_pathway = ref_codon
+                                        for base_pos in codon_permutation:
+                                            mutant_pos = base_change_pos[base_pos]
+                                            mutant_nt = codon[mutant_pos:mutant_pos+1]
+                                            codon_pathway = codon_pathway[:mutant_pos] + mutant_nt + \
+                                                    codon_pathway[mutant_pos+1:]
+
+                                            if Seq(codon_pathway).translate()[0] == ref_aa:
+                                                s_count += 1
+                                            else:
+                                                ns_count += 1
+
+                                    # Create CodonVariant and add to collection
+                                    mutation = CodonVariant(chrom=chrom,
+                                                            gene=gene_key,
+                                                            id="mutation",
+                                                            ref=ref_codon,
+                                                            alt=codon,
+                                                            mutant_freq=census.codon_frequency_for_amino_at(frame, ref_codon_pos, aa, CONFIDENT, codon) / coverage*100,
+                                                            coverage=coverage,
+                                                            pos=(ref_codon_pos - (
+                                                                gene['start'] // 3
+                                                            ) + 1),
+                                                            nt_start_gene=gene['start'],
+                                                            nt_end_gene=gene['end'],
+                                                            nt_start=(ref_codon_pos*3 + frame),
+                                                            nt_end=(ref_codon_pos*3 + frame+2),
+                                                            ref_codon=ref_codon,
+                                                            mutant_codon=codon,
+                                                            ref_aa=ref_aa,
+                                                            mutant_aa=aa,
+                                                            mutant_type=mutation_type,
+                                                            ns_count=ns_count,
+                                                            s_count=s_count
+                                                            )
+                                    var_collect.variants[gene_key][(ref_codon_pos*3 + frame)][codon] = mutation
+        return var_collect
+
+    def to_csv_file(self, offset):
+        """"Build a string representation of our CodonVariant objects
+        (i.e. a csv file)."""
+
+        report = ("#gene,nt position (gene),nt start position,nt end position,ref codon,mutant codon,"
+        "ref AA,mutant AA,coverage,mutant frequency,mutant type,NS count,S count\n")
+
+        for gene in self.variants:
+            for pos in self.variants[gene]:
+                for codon in self.variants[gene][pos]:
+                    report += self.variants[gene][pos][codon].to_csv_entry(offset)
+
+        return report[:-1]
+
+                            
+                                                        
+                                                        
 
 
 
--- quasitools/commands/cmd_mutanttypes.py
+++ quasitools/commands/cmd_mutanttypes.py
@@ -1,7 +1,8 @@
 """
 Copyright Government of Canada 2017
 
-Written by: Camy Tran, National Microbiology Laboratory, Public Health Agency of Canada
+Written by: Camy Tran, National Microbiology Laboratory, 
+            Public Health Agency of Canada
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this work except in compliance with the License. You may obtain a copy of the
@@ -16,13 +17,13 @@ specific language governing permissions and limitations under the License.
 """
 
 import click
-from Bio.Seq import Seq
 from quasitools.cli import pass_context
 from quasitools.parsers.reference_parser import parse_references_from_fasta
 from quasitools.parsers.mapped_read_parser import parse_mapped_reads_from_bam
 from quasitools.parsers.genes_file_parser import parse_genes_file
 from quasitools.nt_variant import NTVariantCollection
-from quasitools.aa_census import AACensus, CONFIDENT
+from quasitools.aa_census import AACensus
+from quasitools.codon_variant import CodonVariantCollection
 
 @click.command('mutanttypes', short_help='Identify the number of non-synonymous and synonymous mutations.')
 @click.argument('bam', required=True, type=click.Path(exists=True))
@@ -36,31 +37,31 @@ from quasitools.aa_census import AACensus, CONFIDENT
 def cli(ctx, bam, reference, offset, genes_file, output, error_rate):
     click.echo("Running mutanttypes command...")
 
-    reference_sequence = parse_references_from_fasta(reference)
+    rs = parse_references_from_fasta(reference)
     mapped_read_collection_arr = []
 
     # Create a MappedReadCollection object
-    for reference in reference_sequence:
-        mapped_read_collection_arr.append(parse_mapped_reads_from_bam(reference, bam))
+    for r in rs:
+        mapped_read_collection_arr.append(parse_mapped_reads_from_bam(r, bam))
 
-    variants = NTVariantCollection.from_mapped_read_collections(error_rate, reference_sequence, 
-                                                                *mapped_read_collection_arr)
-    variants.filter('q30', 'QUAL<30', True)
-    variants.filter('ac5', 'AC<5', True)
-    variants.filter('dp100', 'DP<100', True)
+    variants_obj = NTVariantCollection.from_mapped_read_collections(error_rate, rs, 
+                    *mapped_read_collection_arr)
+    variants_obj.filter('q30', 'QUAL<30', True)
+    variants_obj.filter('ac5', 'AC<5', True)
+    variants_obj.filter('dp100', 'DP<100', True)
 
     click.echo("Creating variants vcf file...")
     vcf_filename = "{}.vcf".format(output.split('.')[0])
     vcf_file = open(vcf_filename, "w")
-    vcf_file.write(variants.to_vcf_file())
+    vcf_file.write(variants_obj.to_vcf_file())
     vcf_file.close()
 
     # Mask the unconfident differences
     for mrc in mapped_read_collection_arr:
-        mrc.mask_unconfident_differences(vcf_filename)
+        mrc.mask_unconfident_differences_from_file(vcf_filename)
 
     # Parse the genes from the gene file
-    genes = parse_genes_file(genes_file, reference_sequence[0].name)
+    genes = parse_genes_file(genes_file, rs[0].name)
 
     # Determine which frames our genes are in
     frames = set()
@@ -71,77 +72,14 @@ def cli(ctx, bam, reference, offset, genes_file, output, error_rate):
     click.echo("Creating AACensus object...")
     aa_census = AACensus(reference, mapped_read_collection_arr, genes, frames)
 
-    codon_permutations = [[[0]], [[0,1],[1,0]], [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]]]
+    click.echo("Creating CodonVariantCollection")
+    codon_variants = CodonVariantCollection.from_aacensus(aa_census, next(iter(frames)))
 
-    report = ("#gene,nt position (gene),nt start position,nt end position,ref codon,mutant codon,"
-              "ref AA,mutant AA,coverage,mutant frequency,mutant type,NS count,S count\n")
-
-    click.echo("Beginning to identify ns/s codons...")
-    for gene in genes:
-        frame = genes[gene]['frame']
-        codon_start = int((genes[gene]['start'] - frame) / 3)
-        codon_end = int((genes[gene]['end'] - frame - 2) / 3)
-
-        for i in range(codon_start, codon_end):
-            coverage = aa_census.coverage_at(frame, i)
-            ref_codon = reference_sequence[0].seq[(i*3+frame): (i*3+frame) + 3].lower()
-            ref_aa = Seq(ref_codon).translate()[0]
-
-            for aa in aa_census.aminos_at(frame, i, CONFIDENT):
-                frequency = aa_census.amino_frequency_at(frame, i, aa, CONFIDENT)
-                if frequency >= 0.01:
-                    for codon in aa_census.amino_to_codons_at(frame, i, aa, CONFIDENT):
-                        if codon != ref_codon:
-                            if aa == ref_aa:
-                                mutation_type = "S"
-                            else:
-                                mutation_type = "NS"
-
-                            base_change_count = sum(1 for c in codon if not c.islower())
-                            base_change_pos = []
-
-                            for codon_pos in range(0, 3):
-                                nucleotide = codon[codon_pos:codon_pos+1]
-                                if nucleotide.upper() == nucleotide:
-                                    base_change_pos.append(codon_pos)
-
-                            ns_count = 0
-                            s_count = 0
-
-                            for codon_permutation in codon_permutations[base_change_count-1]:
-                                codon_pathway = ref_codon
-                                for base_pos in codon_permutation:
-                                    mutant_pos = base_change_pos[base_pos]
-                                    mutant_nt = codon[mutant_pos:mutant_pos+1]
-                                    codon_pathway = codon_pathway[:mutant_pos] + mutant_nt + \
-                                                    codon_pathway[mutant_pos+1:]
-
-                                    if Seq(codon_pathway).translate()[0] == ref_aa:
-                                        s_count += 1
-                                    else:
-                                        ns_count += 1
-
-                            report += "%s,%i-%i,%i,%i,%s,%s,%s,%s,%i,%.2f,%s,%0.4f,%0.4f\n" % \
-                                      ( gene, 
-                                        genes[gene]['start'] + offset,
-                                        genes[gene]['end'] + offset, 
-                                        (i*3 + frame + offset),
-                                        (i*3 + frame+2 + offset), 
-                                        ref_codon, 
-                                        codon, 
-                                        ref_aa, 
-                                        aa, 
-                                        coverage,
-                                        aa_census.codon_frequency_for_amino_at(frame, i, aa, CONFIDENT, codon) / coverage*100,
-                                        mutation_type,
-                                        ns_count / len(codon_permutations[base_change_count-1]),
-                                        s_count / len(codon_permutations[base_change_count-1]) )
+    report = codon_variants.to_csv_file(offset)
 
     csv_file = open(output, "w")
     csv_file.write(report)
     csv_file.close()
 
-
-
-
+    click.echo(report)
 
