1dc5bc4f81e990399c98e4b475c6620b1654876c
camytran <camy.t.tran@gmail.com>
2017-10-11 09:16:19 -0500

    Make pep8 fixes

--- quasitools/parsers/codon_variant_file_parser.py
+++ quasitools/parsers/codon_variant_file_parser.py
@@ -16,8 +16,8 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
-from collections import defaultdict
 from quasitools.codon_variant import CodonVariant, CodonVariantCollection
+
 
 def parse_codon_variants(csv, references):
     """Parse a codon variants csv and build a codon variants object"""
