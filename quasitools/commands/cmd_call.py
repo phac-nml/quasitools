46a180f1f1da7d06d09eaaa34a8755131471dc1b
camytran <camy.t.tran@gmail.com>
2017-11-01 11:17:05 -0500

    Update cmd_call file header

--- quasitools/commands/cmd_call.py
+++ quasitools/commands/cmd_call.py
@@ -1,8 +1,8 @@
 """
 Copyright Government of Canada 2017
 
-Written by: Camy Tran, National Microbiology Laboratory,
-            Public Health Agency of Canada
+Written by: Eric Enns, Cole Peters, Eric Chubaty, Camy Tran,
+            National Microbiology Laboratory, Public Health Agency of Canada
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this work except in compliance with the License. You may obtain a copy of the
