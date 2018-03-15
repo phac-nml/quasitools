8106df320c785eb110487b6f5216f205d9145a79
camytran <camy.t.tran@gmail.com>
2018-03-15 11:15:11 -0500

    Use RG tag for fasta identifier in consensus output

--- quasitools/commands/cmd_consensus.py
+++ quasitools/commands/cmd_consensus.py
@@ -33,11 +33,13 @@ from quasitools.parsers.mapped_read_parser import parse_mapped_reads_from_bam
 @click.option('-p', '--percentage', default=100,
               help='percentage to include base in mixture.')
 @click.option('-i', '--id',
-              help='FASTA sequence identifier.')
+              help='specify default FASTA sequence identifier to be used ' 
+              'for sequences without an RG tag.')
 @click.option('-o', '--output', type=click.File('w'))
 @pass_context
 def cli(ctx, bam, reference, percentage, id, output):
     rs = parse_references_from_fasta(reference)
+    bam_header = pysam.Samfile(bam, "rb").header
 
     if id:
         fasta_id = id
@@ -48,6 +50,9 @@ def cli(ctx, bam, reference, percentage, id, output):
         mrc = parse_mapped_reads_from_bam(r, bam)
 
         conseq = mrc.to_consensus(percentage)
+
+        if hasattr(bam_header, 'RG'):
+            fasta_id = bam_header['RG']
 
         if output:
             output.write('>{0}_{1}_{2}\n{3}'.format(fasta_id, percentage, r.name,
