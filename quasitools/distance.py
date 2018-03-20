afd133647075b0492919379c93a5d948b757ce0b
Matthew Fogel <matthew.fogel@canada.ca>
2018-03-20 09:38:19 -0500

    Begin addressing flake8 issues

--- quasitools/distance.py
+++ quasitools/distance.py
@@ -15,7 +15,6 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
 
-import sys
 import numpy as np
 
 # Quasitools parsers:
@@ -29,9 +28,10 @@ BASES = ['A', 'C', 'T', 'G']
 
 class Distance(object):
 
-    #def __init__(self):
 
-    #end def
+    # def __init__(self):
+
+    # end def
 
     def construct_pileup(self, viral_files, reference_loc):
 
@@ -57,23 +57,23 @@ class Distance(object):
             mrcList = []
             for bam in viral_files:
                 mrcList.append(parse_mapped_reads_from_bam(reference, bam))
-            #end for
+            # end for
 
-            #pileups are a list of dictionaries. The pileup list contains
-            #a list of pileups for each mapped read.
+            # pileups are a list of dictionaries. The pileup list contains
+            # a list of pileups for each mapped read.
             for num in range(0, len(mrcList)):
                 if len(pileup_list) < (num + 1):
-                    #add a new pileup to the pileup list
+                    # add a new pileup to the pileup list
                     pileup_list.append(mrcList[num].pileup(indels=True))
                 else:
-                    #add the positions in the pileup for this mapped read
-                    #that have not been added yet to this pileup (e.g. if you
-                    #have multiple references in the reference file)
+                    # add the positions in the pileup for this mapped read
+                    # that have not been added yet to this pileup (e.g. if you
+                    # have multiple references in the reference file)
                     pileup_list[num] += mrcList[num].pileup(indels=True)
-                #end if
-            #end for
+                # end if
+            # end for
         return pileup_list
-    #end def
+    # end def
 
     def truncate_output(self, pileup_list):
         """
@@ -94,29 +94,30 @@ class Distance(object):
         """
         deletion_list = []
         if len(pileup_list) > 0:
-            #iterate through every position in reference
+            # iterate through every position in reference
             if len(pileup_list[0]) > 0:
                 for position in range(0, len(pileup_list[0])):
-                    #if any pileup at the current position is empty, add to deletion_list
-                    if any((pileup[position] == {} for pileup in pileup_list) or (sum(pileup[position].values())==0 for pileup in pileup_list)):
+                    # if any pileup at the current position is empty, add to deletion_list
+                    if any((pileup[position] == {} for pileup in pileup_list)
+                    or (sum(pileup[position].values())==0 for pileup in pileup_list)):
                         deletion_list.insert(0, position)
-                #end for           
-            #end if
-        #end if
+                # end for
+            # end if
+        # end if
         for position in deletion_list:
             for pileup in pileup_list:
                 del pileup[position]
-            #end for
-        #end for
+            # end for
+        # end for
         return pileup_list
-    #end def
-        
+    # end def
+
 
     def get_distance_matrix(self, pileup_list, normalized, startpos=None, endpos=None):
 
         """
-        Runs the script, calculating the cosine similarity function between viral
-        quasispecies in viral_files.
+        Runs the script, calculating the cosine similarity function between
+        viral quasispecies in viral_files.
 
         INPUT:
             [ARRAY] [pileup_list] - list of pileups - each pileup is
@@ -124,17 +125,17 @@ class Distance(object):
 
             [BOOL] [normalized] - determine whether to normalize data or not
 
-            [INT] [startpos] -starting base position of reference to be compared
-            when calculating cosine similarity.
+            [INT] [startpos] -starting base position of reference to be
+            compared when calculating cosine similarity.
 
-            [INT] [endpos] - last base position of reference to be compared when
-            calculating cosine similarity.
+            [INT] [endpos] - last base position of reference to be compared
+            when calculating cosine similarity.
 
         RETURN:
             Returns a pairwise matrix containing the cosine similarity function
             between all viral quasispecies is returned. The first row and first
             column of the distance matrix contain labels for which quasispecies
-            are to be compared in each cell corresponding to the row and column.
+            are to be compared in each cell corresponding to the row and column
 
         POST:
             When and if the data is normalized a new pileup_list object is used
@@ -142,38 +143,40 @@ class Distance(object):
             function is not affected.
 
         """
-        if normalized==True:
+        if normalized:
             pileup_list = self.normalize_sum_to_one(pileup_list)
-        #end def
+        # end def
 
         baseList = []
-        first = 0 #first position of dictionaries in each pileup_list[i]
-        if startpos != None:
+        first = 0 # first position of dictionaries in each pileup_list[i]
+        if startpos is not None:
             first = startpos
-        #end if
+        # end if
         for num in range(0, len(pileup_list)):
-            last = len(pileup_list[num]) #last pos of dicts in each pileup_list[i]
-            if endpos != None and (endpos + 1) <= last:
+            # last pos of dicts in each pileup_list[i]
+            last = len(pileup_list[num])
+            if endpos is not None and (endpos + 1) <= last:
                 last = endpos + 1
-            #end if
+            # end if
             baseList.append([pileup_list[num][dict].get(base, 0)
             for dict in range(first, last) for base in BASES])
-        #end for
+        # end for
         baseList = np.array(baseList)
         np.set_printoptions(suppress=True)
-        #create distance matrix for csv file
+        # create distance matrix for csv file
         simi_matrix = squareform(1 - pdist(baseList, cosine))
         di = np.diag_indices(len(simi_matrix))
         simi_matrix[di] = 1.0
         simi_matrix = simi_matrix.tolist()
         return simi_matrix
 
-    #end def
+    # end def
 
     def convert_distance_to_csv(self, matrix, file_list):
 
         """
-        Converts a 2D array (cosine similarity matrix) to a csv-formatted string.
+        Converts a 2D array (cosine similarity matrix) to a csv-formatted
+        string
 
         INPUT:
             [ARRAY] [matrix] - 2D array (cosine similarity matrix)
@@ -181,37 +184,40 @@ class Distance(object):
             [TUPLE] [file_list] - files names which represent a pileup
 
         RETURN:
-            [STRING] [csvOut] CSV representation of a pairwise similarity matrix.
+            [STRING] [csvOut] CSV representation of a pairwise similarity
+            matrix
 
         POST:
             [None]
 
         """
-        #(distMatrix[i+1]).insert(0, file_list[i])
-        #convert from 2d array to csv formatted string
-        csvOut='Quasispecies,' + ','.join('%s' % file for file in list(file_list))
+        # (distMatrix[i+1]).insert(0, file_list[i])
+        # convert from 2d array to csv formatted string
+        csvOut = ('Quasispecies,' +
+        ','.join('%s' % file for file in list(file_list)))
         i = 0;
         for arr in matrix:
             csvOut += "\n"
-            #end if
-            csvOut += ','.join([file_list[i]]+['%.08f' % element for element in arr])
+            # end if
+            csvOut += (','.join([file_list[i]] +
+            ['%.08f' % element for element in arr]))
             i+=1;
-        #end for
+        # end for
         return csvOut
-    #end def
+    # end def
 
     def normalize_sum_to_one(self, pileup_list):
 
         """
-        This function calculates the mean of the read counts for each groupings of four
-        bases (A, C, T, G) and subtracts this mean from the individual read counts. this
-        prevents large read counts for a base from inflating the cosine simularity
-        calculation.
+        This function calculates the mean of the read counts for each groupings
+        of four bases (A, C, T, G) and subtracts this mean from the individual
+        read counts. this prevents large read counts for a base from inflating
+        the cosine simularity calculation.
 
         INPUT:
 
-        [ARRAY] [pileup_list] (a list of dictionaries containing read counts for
-        nucleotides at each position corresponding to the reference)
+        [ARRAY] [pileup_list] (a list of dictionaries containing read counts
+        for nucleotides at each position corresponding to the reference)
 
         RETURN: [ARRAY] [pileup_list] (the normalized list of pileups)
 
@@ -219,19 +225,20 @@ class Distance(object):
         """
         new_list = []
         for num in range(0, len(pileup_list)):
-            #get the mean for sample one
+            # get the mean for sample one
             mean = 0
             new_list.append([])
             for i in range(0, len(pileup_list[num])):
-                total = float(np.sum([pileup_list[num][i].get(base,0) for base in BASES]))
-                #normalize the data for all samples
+                curr_pos = [pileup_list[num][i].get(base,0) for base in BASES]
+                total = float(np.sum(curr_pos))
+                # normalize the data for all samples
                 if total>0:
-                    new_list[num].append({
-                    key: (float(value) / total) for (key, value) in pileup_list[num][i].items() })
+                    new_list[num].append({key: (float(value) / total)
+                    for (key, value) in pileup_list[num][i].items() })
                 else:
-                    new_list[num].append({
-                    key: 0 for (key, value) in pileup_list[num][i].items() })
-                #end if
+                    new_list[num].append({key: 0
+                    for (key, value) in pileup_list[num][i].items() })
+                # end if
         return new_list
         '''
         for num in range(0, len(pileup_list)):
@@ -243,4 +250,4 @@ class Distance(object):
                 pileup_list[num][i] = {
                 key: (value - mean) for key, value in pileup_list[num][i].items() }
                 '''
-    #end def
+    # end def
