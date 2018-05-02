9f7a7509742329ade21fae4ecb1cb6efbc998f5f
Matthew Fogel <matthew.fogel@canada.ca>
2018-05-02 13:40:21 -0500

    Change global var in test for patient analyzer

--- tests/test_patient_analyzer.py
+++ tests/test_patient_analyzer.py
@@ -27,7 +27,7 @@ import Bio.SeqIO
 TEST_PATH = os.path.dirname(os.path.abspath(__file__))
 READS = TEST_PATH + "/data/reads_w_K103N.fastq"
 FORWARD = TEST_PATH + "/data/forward.fastq"
-REVERSE = TEST_PATH + "data/reverse.fastq"
+REVERSE = TEST_PATH + "/data/reverse.fastq"
 REFERENCE = TEST_PATH + "/data/hxb2_pol.fas"
 GENES_FILE = TEST_PATH + "/data/hxb2_pol.bed"
 MUTATION_DB = TEST_PATH + "/data/mutation_db.tsv"
