fa4e940383085cf4c45b83d0b1c806c5ce4ddf34
Matthew Fogel <matthew.fogel@canada.ca>
2018-05-11 09:01:12 -0500

    Make quality_control.py compliant with PEP257 for docstrings for method definitions

--- quasitools/quality_control.py
+++ quasitools/quality_control.py
@@ -41,21 +41,18 @@ FAIL_NS = 3
 
 
 class QualityControl():
-
-    """
-    # =============================================================================
-    INIT
-    ----
-
-    Initializes a dictionary containing frequencies that reads were filtered
-    for reasons such as length, score, and ns. Initializes
-    another dictionary that contains status values and keys for the last time
-    passes_filters was called.
-
-    # =============================================================================
-    """
+    """Perform quality control on FASTQ reads."""
 
     def __init__(self):
+        """
+        Create instance of QualityControl class.
+
+        Initialize a dictionary containing frequencies that reads were
+        filtered for reasons such as length, score, and ns. Initialize
+        another dictionary that contains status values and keys for the last
+        time passes_filters was called.
+
+        """
         self.amount_filtered = {}
         self.amount_filtered[LENGTH] = 0
         self.amount_filtered[SCORE] = 0
@@ -65,34 +62,22 @@ class QualityControl():
                        FAIL_SCORE: SCORE,
                        FAIL_NS: NS}
 
-    """
-    # =============================================================================
-
-    GET MEDIAN SCORE
-    -------------------
-
-
-    PURPOSE
-    -------
-
-    Returns the median score of a sequence record.
-
-
-    INPUT
-    -----
-
-    [BIOPYTHON SEQRECORD] [read]
-        The read where the median score will be retrieved from.
-
-    Returns
-    ----
-
-    [INT] [median_score]
-
-    # =============================================================================
-    """
-
     def get_median_score(self, read):
+        """
+        Return the median score of a sequence record.
+
+        INPUT
+        -----
+
+        [BIOPYTHON SEQRECORD] [read]
+            The read where the median score will be retrieved from.
+
+        RETURN
+        ------
+
+        [INT] [median_score]
+
+        """
         length = len(read.seq)
         last_pos = length - 1
 
@@ -106,85 +91,59 @@ class QualityControl():
 
         return median_score
 
-    """
-    # =============================================================================
-
-    GET MEAN SCORE
-    -------------------
-
-
-    PURPOSE
-    -------
-
-    Returns the mean score of a sequence record.
-
-
-    INPUT
-    -----
-
-    [BIOPYTHON SEQRECORD] [read]
-        The read where the mean score will be retrieved from.
-
-    Returns
-    ----
-
-    [INT] [mean_score]
-
-    # =============================================================================
-    """
-
     def get_mean_score(self, read):
+        """
+        Return the mean score of a sequence record.
 
+        INPUT
+        -----
+
+        [BIOPYTHON SEQRECORD] [read]
+            The read where the mean score will be retrieved from.
+
+        RETURN
+        ------
+
+        [INT] [mean_score]
+
+        """
         mean_score = (float(sum(read.letter_annotations['phred_quality'])) /
                       float(len(read.letter_annotations['phred_quality'])))
 
         return mean_score
 
-    """
-    # =============================================================================
-
-    TRIM READ
-    ---------
-
-
-    PURPOSE
-    -------
-
-    Trims the read in an iterative manner until it meets all the filtering
-    criteria. The specifications of the filtering parameters can be found in
-    this file.
-
-
-    INPUT
-    -----
-
-    [BIOPYTHON SEQRECORD] [read]
-        The read to iteratively trim until it meets filtering criteria.
-
-    [(FILTER -> VALUE) DICTIONARY] [filters]
-        The filtering critiera, as a dictionary of (filter, value) pairs, which
-        will be tested against the read after each iteration of read trimming.
-
-    POST
-    ----
-
-    The read will first be evaluated against the filtering criteria. If the
-    read passes the filtering criteria, then the read will be returned without
-    modification. However, if the read does not meet the filtering criteria, it
-    will be trimmed iteratively.
-
-    In the latter case, the read will be trimmed iteratively, base by base,
-    from the tail end of the read, starting at position [len(read) - 1], until
-    either the read meets the filtering criteria, or the read is too short.
-
-    The trimmed and modified read will still be returned if it fails to be
-    improved with iterative trimming.
-
-    # =========================================================================
-    """
-
     def trim_read(self, read, filters):
+        """
+        Iteratively trim the read until it meets all the filtering criteria.
 
+        INPUT
+        -----
+
+        [BIOPYTHON SEQRECORD] [read]
+            The read to iteratively trim until it meets filtering criteria.
+
+        [(FILTER -> VALUE) DICTIONARY] [filters]
+            The filtering critiera, as a dictionary of (filter, value) pairs,
+            which will be tested against the read after each iteration of read
+            trimming.
+
+        POST
+        ----
+
+        The read will first be evaluated against the filtering criteria. If the
+        read passes the filtering criteria, then the read will be returned
+        without modification. However, if the read does not meet the filtering
+        criteria, it will be trimmed iteratively.
+
+        In the latter case, the read will be trimmed iteratively, base by base,
+        from the tail end of the read, starting at position [len(read) - 1],
+        until either the read meets the filtering criteria, or the read is too
+        short.
+
+        The trimmed and modified read will still be returned if it fails to be
+        improved with iterative trimming.
+
+        """
         length = len(read.seq)
         len_cutoff = filters.get(LENGTH_CUTOFF)
         status = self.passes_filters(read, filters)
@@ -197,43 +156,28 @@ class QualityControl():
 
         return read
 
-    """
-    # =========================================================================
-
-    MASK_READ
-    ---------
-
-
-    PURPOSE
-    -------
-
-    Masks the nucleotide of all low quality positions in the read with an
-    MASK_CHARACTER.
-
-
-    INPUT
-    -----
-
-    [BIOPYTHON SEQRECORD] [read]
-        The read to mask low quality positions within.
-
-    [(FILTER -> VALUE) DICTIONARY] [filters]
-        The filtering criteria, as a dictionary of (filter, value) pairs. The
-        minimum quality score will be taken from this dictionary as the value
-        of the MIN_READ_QUAL key.
-
-
-    POST
-    ----
-
-    The nucleotide positions in the passed read will be masked with
-    a MASK_CHARACTER if their PHRED quality score is below the minimum.
-
-    # =========================================================================
-    """
-
     def mask_read(self, read, filters):
+        """
+        Mask the nucleotide of all low quality positions in the read.
 
+        INPUT
+        -----
+
+        [BIOPYTHON SEQRECORD] [read]
+            The read to mask low quality positions within.
+
+        [(FILTER -> VALUE) DICTIONARY] [filters]
+            The filtering criteria, as a dictionary of (filter, value) pairs.
+            The minimum quality score will be taken from this dictionary as the
+            value of the MIN_READ_QUAL key.
+
+        POST
+        ----
+
+        The nucleotide positions in the passed read will be masked with
+        a MASK_CHARACTER if their PHRED quality score is below the minimum.
+
+        """
         scores = list(read.letter_annotations['phred_quality'])
         minimum = int(filters.get(MIN_READ_QUAL))
 
@@ -252,43 +196,30 @@ class QualityControl():
 
         return
 
-    """
-    # =========================================================================
+    def passes_filters(self, read, filters):
+        """
+        Determine whether or not the read passes all the filtering criteria.
 
-    PASSES FILTERS
-    --------------
+        INPUT
+        -----
 
+        [BIOPYTHON SEQRECORD] [read]
+            The read that will be evaluated using the filtering criteria.
 
-    PURPOSE
-    -------
+        [(FILTER -> VALUE) DICTIONARY] [filters]
+            The filtering criteria, as a dictionary of (filter, value) pairs,
+            all of which will be tested against the read.
 
-    Determines whether or not the read passes all the filtering criteria.
+        RETURN
+        ------
 
-
-    INPUT
-    -----
-
-    [BIOPYTHON SEQRECORD] [read]
-        The read that will be evaluated using the filtering criteria.
-
-    [(FILTER -> VALUE) DICTIONARY] [filters]
-        The filtering criteria, as a dictionary of (filter, value) pairs, all
-        of which will be tested against the read.
-
-    RETURN
-    ------
-
-    [INT] [result]
+        [INT] [result]
         PASS: the read passes all the filtering criteria
         FAIL_LENGTH: the read fails due to read length
         FAIL_SCORE: the read fails due to read score
         FAIL_NS: the read fails due to MASK_CHARACTERs
 
-    # =========================================================================
-    """
-
-    def passes_filters(self, read, filters):
-
+        """
         length_cutoff = filters.get(LENGTH_CUTOFF)
         median_cutoff = filters.get(MEDIAN_CUTOFF)
         mean_cutoff = filters.get(MEAN_CUTOFF)
@@ -308,50 +239,37 @@ class QualityControl():
 
         return PASS
 
-    """
-    # =========================================================================
-
-    FILTER READS
-    -----------
-
-
-    PURPOSE
-    -------
-
-    Filters reads according to a variety of filtering criteria.
-
-
-    INPUT
-    -----
-
-    [FILE LOCATION] [reads_location]
-        The file location of a FASTQ-encoded reads file.
-
-    [FILE LOCATION] [output_location]
-        The output location of the filtered reads.
-
-    [(FILTER -> VALUE) DICTIONARY] [filters]
-        The filtering criteria, as a dictionary of (filter, value) pairs, all
-        of which will be tested against the read.
-
-    RETURN
-    ------
-
-    Returns true if function completed successfully.
-
-    POST
-    ----
-
-    The reads that pass the filtering criteria will be written to
-    [output_location]. The self.amount_filtered dict will
-    contain the frequencies that a read was filtered (excluded from being
-    written to the output file) due to failing the filtering criteria.
-
-    # =========================================================================
-    """
-
     def filter_reads(self, reads_location, output_location, filters):
+        """
+        Filter reads according to a variety of filtering criteria.
 
+        INPUT
+        -----
+
+        [FILE LOCATION] [reads_location]
+            The file location of a FASTQ-encoded reads file.
+
+        [FILE LOCATION] [output_location]
+            The output location of the filtered reads.
+
+        [(FILTER -> VALUE) DICTIONARY] [filters]
+            The filtering criteria, as a dictionary of (filter, value) pairs,
+            all of which will be tested against the read.
+
+        RETURN
+        ------
+
+        Returns true if function completed successfully.
+
+        POST
+        ----
+
+        Writes FASTQ reads that pass the filtering criteria to
+        [output_location]. The self.amount_filtered dict will
+        contain the frequencies that a read was filtered (excluded from being
+        written to [output_location]) due to failing the filtering criteria.
+
+        """
         filtered_reads_file = open(output_location, "w+")
         reads = Bio.SeqIO.parse(reads_location, "fastq")
 
@@ -379,34 +297,16 @@ class QualityControl():
 
         return True
 
-    """
-    # =========================================================================
-
-    GET AMOUNT FILTERED
-    -----------
-
-
-    PURPOSE
-    -------
-
-    Returns the dictionary containing the frequencies that the read was
-    filtered (excluded from the output file) due to filtering criteria. E.g.
-    Records how many times the read was filtered due to length and score.
-
-    INPUT
-    -----
-
-    None.
-
-
-    POST
-    ----
-
-    None.
-
-    # =========================================================================
-    """
-
     def get_amount_filtered(self):
+        """
+        Get the frequencies that the read was filtered.
 
+        RETURN
+        ------
+
+        [(FILTER -> FREQUENCY) DICTIONARY] [self.amount_filtered]
+            The dictionary containing the frequencies that the read was
+            filtered due to filtering criteria.
+
+        """
         return self.amount_filtered
