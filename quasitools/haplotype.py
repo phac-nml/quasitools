1f389df190e99d8b5af9bfe9b6e4defc982618ec
Eric Marinier <emarinier@gmail.com>
2018-07-18 14:31:57 -0500

    Additional function comments.

--- quasitools/haplotype.py
+++ quasitools/haplotype.py
@@ -56,11 +56,34 @@ class Haplotype:
 
         return False
 
-"""
-# =============================================================================
-# =============================================================================
-"""
 def build_from_reads(reads_location, consensus):
+    """
+    # ========================================================================
+
+    BUILD FROM READS
+
+
+    PURPOSE
+    -------
+
+    Builds a list of Haplotype objects from aligned FASTA reads.
+
+
+    INPUT
+    -----
+
+    [FILE LOCATION] [reads_location]
+        The location of the aligned FASTA reads.
+
+
+    RETURN
+    ------
+
+    [HAPLOTYPE LIST]
+        A list of Haplotype objects, defined by the align FASTA reads.
+
+    # ========================================================================
+    """
 
     haplotypes = {} # (sequence, Haplotype)
 
@@ -84,21 +107,69 @@ def build_from_reads(reads_location, consensus):
 
     return haplotypes_sorted
 
-"""
-# =============================================================================
-# =============================================================================
-"""
 def sort_haplotypes(haplotypes):
+    """
+    # ========================================================================
+
+    SORT HAPLOTYPES
+
+
+    PURPOSE
+    -------
+
+    Sorts a list of haplotypes according their number of mutations from the
+    consensus sequence.
+
+
+    INPUT
+    -----
+
+    [HAPLOTYPE LIST] [haplotypes]
+        The list of haplotypes to sort.
+
+
+    RETURN
+    ------
+
+    [HAPLOTYPE LIST]
+        A list of sorted haplotypes, according to their number of mutations
+        from the consensus sequence of all the sequences.
+
+    # ========================================================================
+    """
 
     sorted_haplotypes = sorted(haplotypes, key=attrgetter('mutations'), reverse=False)
 
     return sorted_haplotypes
 
-"""
-# =============================================================================
-# =============================================================================
-"""
 def build_distiance_matrix(haplotypes):
+    """
+    # ========================================================================
+
+    BUILD DISTANCE MATRIX
+
+
+    PURPOSE
+    -------
+
+    Builds a distance matrix of all the haplotypes.
+
+
+    INPUT
+    -----
+
+    [HAPLOTYPE LIST] [haplotypes]
+        The list of Haplotypes from which to build the distance matrix.
+
+
+    RETURN
+    ------
+
+    [2D ARRAY]
+        A distance matrix of the passed haplotypes.
+
+    # ========================================================================
+    """
 
     haplotypes = sort_haplotypes(haplotypes)
     x = len(haplotypes)
@@ -112,22 +183,73 @@ def build_distiance_matrix(haplotypes):
             matrix[i][j] = calculate_distance(haplotypes[i], haplotypes[j])
 
     return matrix
-"""
-# =============================================================================
-# =============================================================================
-"""
+
 def calculate_distance(haplotype1, haplotype2):
+    """
+    # ========================================================================
+
+    CALCULATE DISTANCE
+
+
+    PURPOSE
+    -------
+
+    Calculates the distance between two haplotypes.
+
+
+    INPUT
+    -----
+
+    [HAPLOTYPE] [haplotype1]
+        The first of two haplotypes to calculate the distance between.
+
+    [HAPLOTYPE] [haplotype2]
+        The second of two haplotypes to calculate the distance between.
+
+
+    RETURN
+    ------
+
+    [FLOAT]
+        The genetic distance between the two passed haplotypes.
+
+    # ========================================================================
+    """
 
     hamming_distance = calculate.hamming_distance(haplotype1.sequence, haplotype2.sequence)
     genetic_distance = hamming_distance / len(haplotype1.sequence)
 
     return genetic_distance
 
-"""
-# =============================================================================
-# =============================================================================
-"""
 def calculate_total_clones(haplotypes):
+    """
+    # ========================================================================
+
+    CALCULATE TOTAL CLONES
+
+
+    PURPOSE
+    -------
+
+    Calculates the total number of clones accross multiple haplotypes. Note
+    that there may be multiple clones associated with each haplotype.
+
+
+    INPUT
+    -----
+
+    [HAPLOTYPE LIST] [haplotypes]
+        The list of Haplotypes from which to build the distance matrix.
+
+
+    RETURN
+    ------
+
+    [INT]
+        The total number of clones accross all the haplotypes in the list.
+
+    # ========================================================================
+    """
 
     total = 0
 
@@ -137,11 +259,51 @@ def calculate_total_clones(haplotypes):
 
     return total
 
-"""
-# =============================================================================
-# =============================================================================
-"""
 def build_counts(haplotypes):
+    """
+    # ========================================================================
+
+    BUILD COUNTS
+
+
+    PURPOSE
+    -------
+
+    Builds the a list of the counts of each haplotype in a passed haplotype
+    list.
+
+    Example:
+
+    haplotype1.sequence = "AAA"
+    haplotype1.count = 3
+
+    haplotype1.sequence = "CGC"
+    haplotype1.count = 5
+
+    haplotype1.sequence = "TCC"
+    haplotype1.count = 1
+
+    haplotypes = [haplotype1, haplotype2, haplotype3]
+
+    build_counts(haplotypes) -> [3, 5, 1]
+
+
+    INPUT
+    -----
+
+    [HAPLOTYPE LIST] [haplotypes]
+        The list of Haplotypes.
+
+
+    RETURN
+    ------
+
+    [INT LIST]
+        A list of the counts of each haplotype, in the same order as the
+        original haplotype list.
+
+    # ========================================================================
+    """
 
     haplotypes = sort_haplotypes(haplotypes)
     counts = []
